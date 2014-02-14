bda75cca546ce580ad2542d161cbd4e3d9a012eb
mindscapehq <mindscapehq@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-14 01:04:35 +0000

    Version bump
    git-svn-id: https://plugins.svn.wordpress.org/raygun4wp/tags/1.2.0.0@857576 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ dash.php
@@ -0,0 +1,11 @@
+<style type='text/css'>
+#rgFrame
+{
+    display: block;
+    margin: auto;
+    width: 100%;
+    height: 840px !important;
+}
+</style>
+
+<iframe id="rgFrame" src='http://app.raygun.io?utm_source=wordpress&utm_medium=admin&utm_campaign=raygun4wp' frameborder="0" style="overflow:hidden;height:100%;width:100%" height="900px" width="100%"></iframe>
